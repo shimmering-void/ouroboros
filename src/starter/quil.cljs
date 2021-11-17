9a516dd85c7e15502937fcbfe9a053516a556014
Ben Follington <bfollington@protonmail.com>
2021-11-17 12:32:06 +1000

    Swirls

--- src/starter/quil.cljs
+++ src/starter/quil.cljs
@@ -27,30 +27,64 @@
 
 (defn setup []
   ;; EXAMPLE CODE, DO NOT HARDCODE YOUR FEATURES
-  (f/register-features {:dark true :another-feature "yes"})
 
-  (q/frame-rate 30)
 
-  ; setup function returns initial state
-  {:t 0
-   :green (f/fx-rand)})
+  (q/frame-rate 60)
+
+  ;; (q/color-mode :hsb)
+
+
+  (let [omega (f/fx-rand)
+        gamma (f/fx-rand)
+        delta (f/fx-rand)]
+    (f/register-features {:twitchy (< delta 0.05)
+                          :dizzy (< gamma 0.05)
+                          :bouncy (and (> delta 0.3) (< gamma 0.3))
+                          :loopy (< (q/abs (- delta gamma)) 0.1)
+                          :swirly (> gamma 0.7)})
+    {:t 0
+     :omega omega
+     :gamma gamma
+     :delta delta}))
 
 (defn update-state [state]
   (merge state {:t (inc (:t state))}))
 
-(defn draw-state [{:keys [t green]}]
-  (let [t' (/ t 10)]
+(defn draw-state [{:keys [t omega gamma delta]}]
+  (let [t' (/ t (+ 8 (* gamma 32)))]
     ; clear screen
-    (q/fill 255 255 255)
-    (q/stroke-weight 0)
-    (q/background 0)
-    ; fxhash functions
-    (q/text (str "fxhash=" (f/fx-hash)) (x 2) (y 2))
-    (q/text (str "fxrand=" (f/fx-rand)) (x 2) (y 4))
+    (q/fill 0 0 0)
+    (q/stroke-weight (+ 1 (* delta 4)))
+
+    (when (> (q/sin (/ t' 16)) 0.98)
+      (q/blend-mode :blend)
+      (q/background 0 0 0 32))
+
+    (q/blend-mode :screen)
+
+    ;; (q/blend-mode :blend)
+    ;; (q/text (str "gamma=" gamma) (x 2) (y 2))
+    ;; (q/text (str "delta=" delta) (x 2) (y 4))
+    ;; (q/text (str "omega=" omega) (x 2) (y 6))
+    ;; (q/blend-mode :screen)
+
+    (let [angle (* (+ (* gamma (q/sin t')) (* delta (q/cos t'))) 2)
+          length (* (y 100) (q/sin (* gamma t')) (q/cos (* delta t')))
+          [lx ly] [(+ (x 45) (x (* 45 (q/cos (/ t' (* 3 delta)))))) (+ (y 45) (y (* 45 (q/sin (/ t' (* 3 gamma))))))]]
+      (apply q/stroke [(* 255 (+ omega (* delta (q/cos t'))))
+                       (+ 63 (* 192 (q/sin (* delta t'))))
+                       (+ 128 (* 128 (q/cos (* delta t'))))])
+      (q/line [(+ lx (* (/ length 2) -1 (q/cos angle))) (+ ly (* (/ length 2) -1 (q/sin angle)))]
+              [(+ lx (* (/ length 2) (q/cos angle))) (+ ly (* (/ length 2) (q/sin angle)))])
+
+      (comment (let [[w h] dims]
+                 (apply q/fill [(* 255 (q/sin t')) (* 255 omega) (* 255 (q/cos t'))])
+                 (q/ellipse (/ w 2) (/ h 2) (x 10) (y 10))))))
+
+
+
     ; draw resolution independent circle
-    (let [[w h] dims]
-      (q/fill (* 255 (q/sin t')) (* 255 green) (* 255 (q/cos t')))
-      (q/ellipse (/ w 2) (/ h 2) (x 10) (y 10)))))
+  )
 
 ; this function is called in index.html
 (defn ^:export run-sketch []
@@ -61,7 +95,7 @@
     :setup setup
     :update update-state
     :draw draw-state
-    :key-pressed (u/save-image "export.png")
+    ;; :key-pressed (u/save-image "export.png")
     :middleware [m/fun-mode]))
 
 ;; start is called by init and after code reloading finishes
